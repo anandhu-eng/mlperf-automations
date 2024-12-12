4332f6eb5081c4f05f6c3b8633aef5b2b22d1daf
anandhu-eng <anandhukicks@gmail.com>
2024-12-12 14:37:14 +0530

    changes for llama3-402b

--- script/app-mlperf-inference-mlcommons-python/customize.py
+++ script/app-mlperf-inference-mlcommons-python/customize.py
@@ -497,6 +497,25 @@ def get_run_cmd_reference(
 
         if env.get('CM_ACTIVATE_RGAT_IN_MEMORY', '') == "yes":
             cmd += " --in-memory "
+    
+    elif "llama3" in env['CM_MODEL']:
+        env['RUN_DIR'] = os.path.join(
+            env['CM_MLPERF_INFERENCE_SOURCE'],
+            "graph",
+            "R-GAT")
+        
+        cmd = env['CM_PYTHON_BIN_WITH_PATH'] + " main.py " \
+            " --scenario " + env['CM_MLPERF_LOADGEN_SCENARIO'] + \
+            " --dataset-path " + env['CM_DATASET_LLAMA3_PATH'] + \
+            env['CM_MLPERF_LOADGEN_EXTRA_OPTIONS'] + \
+            scenario_extra_options + mode_extra_options + \
+            " --output-log-dir " + env['CM_MLPERF_OUTPUT_DIR'] + \
+            ' --dtype ' + env['CM_MLPERF_MODEL_PRECISION'] + \
+            " --model-path " + env['CM_ML_MODEL_LLAMA3_CHECKPOINT_PATH'] + \
+            " --vllm "
+        
+        cmd = cmd.replace("--count", "--total-sample-count")
+        
 
     if env.get('CM_NETWORK_LOADGEN', '') in ["lon", "sut"]:
         cmd = cmd + " " + "--network " + env['CM_NETWORK_LOADGEN']
---
+++ script/get-ml-model-llama3/customize.py
@@ -0,0 +1,27 @@
+from cmind import utils
+import os
+
+
+def preprocess(i):
+
+    os_info = i['os_info']
+    env = i['env']
+
+    path = env.get('LLAMA3_CHECKPOINT_PATH', '').strip()
+    if path == '' or not os.path.exists(path):
+        env['CM_TMP_REQUIRE_DOWNLOAD'] = 'yes'
+
+    return {'return': 0}
+
+
+def postprocess(i):
+
+    env = i['env']
+    if env.get('LLAMA3_CHECKPOINT_PATH', '') == '':
+        env['LLAMA3_CHECKPOINT_PATH'] = env['CM_ML_MODEL_PATH']
+    else:
+        env['CM_ML_MODEL_PATH'] = env['LLAMA3_CHECKPOINT_PATH']
+    env['CM_ML_MODEL_LLAMA3_CHECKPOINT_PATH'] = env['LLAMA3_CHECKPOINT_PATH']
+    env['CM_GET_DEPENDENT_CACHED_PATH'] = env['CM_ML_MODEL_PATH']
+
+    return {'return': 0}
