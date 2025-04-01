af7a11171fc5694f5ce5bcb017ce9298c6726c30
anandhu-eng <anandhusooraj011@gmail.com>
2025-04-01 22:03:48 +0530

    revert the test commit

--- script/get-mlperf-inference-sut-description/detect_memory.sh
+++ script/get-mlperf-inference-sut-description/detect_memory.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ ${MLC_SUDO_USER} == "yes" ]]; then
-  echo "${MLC_SUDO} dmidecode -t memory > ${MLC_MEMINFO_FILE}"
   ${MLC_SUDO} dmidecode -t memory > ${MLC_MEMINFO_FILE}
-  echo "dmidecode over"
 fi
 # test $? -eq 0 || return $?
 # commented as it is not working properly inside docker
