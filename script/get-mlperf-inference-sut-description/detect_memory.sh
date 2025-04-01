4815f4cd8dd4769b22850da2b0e12fa88b6a2086
anandhu-eng <anandhusooraj011@gmail.com>
2025-04-01 22:11:08 +0530

    print warning if dmidecode is not measured

--- script/get-mlperf-inference-sut-description/detect_memory.sh
+++ script/get-mlperf-inference-sut-description/detect_memory.sh
@@ -3,5 +3,5 @@
 if [[ ${MLC_SUDO_USER} == "yes" ]]; then
   ${MLC_SUDO} dmidecode -t memory > ${MLC_MEMINFO_FILE}
 fi
-# test $? -eq 0 || return $?
+test $? -eq 0 || echo "Warning: Memory info is not recorded"
 # commented as it is not working properly inside docker
